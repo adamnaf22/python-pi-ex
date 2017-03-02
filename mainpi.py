43805c85860b2d6c2e8506454b27562b2beddd8e
tony <adamnaftalin222@gmail.com>
2017-03-02 09:28:21 -0500

    changed main.pi

--- mainpi.py
+++ mainpi.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
-print("Hello World")
-# python-pi-ex
-This is an example project to demonstrate the use of git on Ubuntu.
+
+
+def main(msg):
+    print(msg)
+
+
+main("Hello People")
